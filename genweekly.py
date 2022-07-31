4c20638c15174940404d8adbb9af025c99e5c85a
nixnull <nixnull@protonmail.com>
2022-07-31 11:51:53 -0500

    Jul 31

--- genweekly.py
+++ genweekly.py
@@ -2,6 +2,7 @@
 from sys import argv
 from csv import reader
 from datetime import datetime, timezone 
+import json
 
 isTest = "-t" in argv
 isReport = "-r" in argv
@@ -21,3 +22,14 @@ time_str = now.strftime('%B %d, %Y')
 fancy_time = time_str
 
 print(fancy_time)
+
+# Opening JSON file
+with open('players/nix.json', 'r') as openfile:
+ 
+    # Reading from json file
+    json_object = json.load(openfile)
+    print(json_object["name"])
+    print(json_object["birthday"])
+ 
+print(json_object)
+print(type(json_object))
